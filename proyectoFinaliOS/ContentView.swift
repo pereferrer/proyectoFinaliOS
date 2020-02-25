4f347dfd68b25b0f7d0acf0976be39fa92c4f5a3
Pere Josep Ferrer Ventura <pere.ferrer.pf@gmail.com>
2020-02-25 12:01:51 +0100

    Initial Commit

---
+++ proyectoFinaliOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  proyectoFinaliOS
+//
+//  Created by Pere Josep Ferrer Ventura on 25/02/2020.
+//  Copyright © 2020 Pere Josep Ferrer Ventura. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
